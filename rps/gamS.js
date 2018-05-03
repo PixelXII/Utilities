3137005f92d22a1455f98229624bbbfc05f878d9
altD <35429528+PixelXII@users.noreply.github.com>
2018-05-03 07:32:41 -0700

    Update gamS.js

--- rps/gamS.js
+++ rps/gamS.js
@@ -7,31 +7,38 @@ function setO() {
 
 var tok;
 function doG(num) {
-  var noo = Math.floor((Math.random() * 10) + 1);
-  if(noo < 3) { 
-    noo = 1;
-  }
-  if(noo > 3 && noo < 6) {
-    noo = 2;
-  }
-  if(noo > 6) {
-    noo = 3;
-  }
-  console.log(noo + ' is noo.');
-  console.log(num + ' is num.');
-  if(noo == num) {
-    output =  'You won ' + Math.ceil(noo) + ' tokens!';
-    tok = tok + Math.ceil(noo);
-  } 
   if(num === 'account') {
     output = 'There are ' + tok + ' tokens in your account. Play more to win!';
-  }
-  if(num > noo || num < noo) {
-    output = 'You lost! ' + Math.ceil(noo/2) + ' tokens have been taken out of your account.';
-    tok = tok - Math.ceil(noo/2);
+  } else if(num === 'reset') {
+    output = "Are you sure you want to reset your account? Press 'Reset' again to confirm!";
+    if(num === reset) {
+      output = "Your account has been reset."
+      tok = 0;
+    }
   } else {
-    output = "You won " + Math.ceil(noo) + " tokens!";
-    tok = tok + Math.ceil(noo);
-  }
+   var noo = Math.floor((Math.random() * 10) + 1);
+   if(noo < 3) { 
+      noo = 1;
+    }
+   if(noo > 3 && noo < 6) {
+     noo = 2;
+    }
+   if(noo > 6) {
+      noo = 3;
+   }
+   console.log(noo + ' is noo.');
+   console.log(num + ' is num.');
+   if(noo == num) {
+      output =  'You won ' + Math.ceil(noo) + ' tokens!';
+      tok = tok + Math.ceil(noo);
+    } 
+    if(num > noo || num < noo) {
+      output = 'You lost! ' + Math.ceil(noo/2) + ' tokens have been taken out of your account.';
+      tok = tok - Math.ceil(noo/2);
+    } else {
+      output = "You won " + Math.ceil(noo) + " tokens!";
+      tok = tok + Math.ceil(noo);
+    }
   setO()
+  }
 }
