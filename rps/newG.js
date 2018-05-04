fa98f7a49c50bfc2fe48e276bac193fbe51567e5
altD <35429528+PixelXII@users.noreply.github.com>
2018-05-03 19:33:26 -0700

    Update newG.js

--- rps/newG.js
+++ rps/newG.js
@@ -1,1 +1,27 @@
+var stuff = document.getElementById('stuff');
+var output;
+var outcome;
+function setO() {
+  document.getElementById('stuff').innerHTML = output;
+  console.log(output)
+}
 
+var tok = 0;
+function doG(num) {
+    if(num =! null) {
+      var j = Math.random();
+      if(j < 0.5) {
+        outcome = 'win'
+      } else {
+        outcome = 'lose'
+      }
+      
+      if(outcome === 'lose') {
+        output = "You lose!"
+        tok--;
+      } else {
+        output = "You win!"
+        tok++;
+      }
+          
+}
