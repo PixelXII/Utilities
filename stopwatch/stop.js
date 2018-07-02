327ec6df8a4de8b3725d16745e0e2f1a07bc7a97
altD <35429528+PixelXII@users.noreply.github.com>
2018-07-02 16:43:51 -0700

    Update stop.js

--- stopwatch/stop.js
+++ stopwatch/stop.js
@@ -1,8 +1,9 @@
 var time = 0;
 var startWatch;
 var stop = document.getElementById('stopwatch')
+stop.style.fontSize = '24px';
 function increaseTime() {
-  time = time + 0.01
+  time = time + 0.1
   stop.style.background = '#'+Math.floor(Math.random()*16777215).toString(16);
   stop.innerHTML = time
 }
