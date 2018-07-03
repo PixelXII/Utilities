ce12f37ddd3e919831449b789a7a1b2586748c4f
altD <35429528+PixelXII@users.noreply.github.com>
2018-07-02 17:59:26 -0700

    Update stop.js

--- stopwatch/stop.js
+++ stopwatch/stop.js
@@ -17,7 +17,7 @@ function loadPage() {
     } else if(hours === 24) {
       document.getElementById('stopText').innerHTML = "oh, why have you left me on this long!! my battery's almost dead!!"
     }
-    text.innerHTML = hours+':'+minutes+':'+seconds
+    text.innerHTML = hours+' hours, '+minutes+' minutes, and '+seconds+' seconds.'
   }
   start = function() {
     startWatch = setInterval(function() { increaseTime() }, 1000)
