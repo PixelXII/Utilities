efb787d199adeb403e4221b4d8cea30eb47d92e3
altD <35429528+PixelXII@users.noreply.github.com>
2018-07-02 17:05:41 -0700

    Update stop.js

--- stopwatch/stop.js
+++ stopwatch/stop.js
@@ -1,19 +1,25 @@
-var time = 0;
-var startWatch;
-var colours;
-var stop = document.getElementById('stopwatch')
-stop.style.fontSize = '48px';
-function increaseTime() {
-  time = time + 0.1
-  stop.innerHTML = Math.round(100*time)/100;
+function loadPage() {
+  var time = 0;
+  var startWatch;
+  var colours;
+  var stop = document.getElementById('stopwatch')
+  stop.style.fontSize = '48px';
+  stop.style.display = "block"
+  document.getElementById('warning').style.display = "none"
+  function increaseTime() {
+    time = time + 0.1
+    stop.innerHTML = Math.round(100*time)/100;
+  }
+  function start() {
+    startWatch = setInterval(function() { increaseTime() }, 100)
+    colors = setInterval(function() { document.body.style.background = '#'+Math.floor(Math.random()*16777215).toString(16); }, 10)
+  }
+  function stopWatch() {
+    clearInterval(startWatch)
+    clearInterval(colors)
+  }
 }
 
-function start() {
-  startWatch = setInterval(function() { increaseTime() }, 100)
-  colors = setInterval(function() { document.body.style.background = '#'+Math.floor(Math.random()*16777215).toString(16); }, 10)
-}
-
-function stopWatch() {
-  clearInterval(startWatch)
-  clearInterval(colors)
+document.onload = function() {
+  setTimeout(function() { loadPage() }, 3000)
 }
