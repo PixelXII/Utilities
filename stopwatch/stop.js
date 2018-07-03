d5799918d409a33332ed34e60292f7981dab9e4e
altD <35429528+PixelXII@users.noreply.github.com>
2018-07-02 17:44:06 -0700

    Update stop.js

--- stopwatch/stop.js
+++ stopwatch/stop.js
@@ -1,32 +1,24 @@
-var time = 0;
-var startWatch;
-var colours;
 var stop = document.getElementById('stopwatch')
-var start;
-var stopWatch;
+var start, stopWatch, minutes = 0, time = 0, colors, startWatch, seconds;
 stop.style.display = "none"
 setTimeout(function() { loadPage() }, 3000)
 
 function loadPage() {
   stop.style.display = "block"
   document.getElementById('warning').style.display = "none"
- /* function increaseTime() {
-    time = time + 0.1
-    document.getElementById('stopText').innerHTML = Math.round(100*time)/100;
+  function increaseTime() {
+    seconds++
+    minutes = Math.floor(time / 60);
+    seconds = time - minutes * 60;
+    document.getElementById('stopText').innerHTML = minutes+':'+seconds
   }
   start = function() {
-    startWatch = setInterval(function() { increaseTime() }, 100)
+    startWatch = setInterval(function() { increaseTime() }, 1000)
+    colors = setInterval(function() { document.body.style.background = '#'+Math.floor(Math.random()*16777215).toString(16); }, 10)
   }
+  
   stopWatch = function() {
     clearInterval(startWatch)
     clearInterval(colors)
-  } */
-  var seconds = new Date().getTime(), last = seconds,
-
-intrvl = setInterval(function() {
-    var now = new Date().getTime();
-    last = now;
-    document.getElementById('stopText').innerHTML = now - seconds;
-    colors = setInterval(function() { document.body.style.background = '#'+Math.floor(Math.random()*16777215).toString(16); }, 10)
-  }, 100);
+  } 
 }
